482a3193dc6502aeca4574ef4e02de00bd82d4ed
JhonEdinson-Shongo <jeca010198@gmail.com>
2025-03-04 15:15:24 -0500

    Se añade DU

--- src/config/global.js
+++ src/config/global.js
@@ -77,7 +77,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/86120363_CF2.pdf',
+        download: 'downloads/86120363_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
